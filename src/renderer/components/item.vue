3ffe184e74ed70bb211a8face404a17c133f059d
482F <482F.git.mail@482f.net>
2022-02-18 00:00:00 +0900

    項目を表示するためのコンポーネントを作成

---
+++ src/renderer/components/item.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-list-item link class="f-item">
+    <v-list-item-avatar left>
+      <v-icon :icon="icon" />
+    </v-list-item-avatar>
+    <v-list-item-header>
+      <v-list-item-title v-text="name" />
+      <v-list-item-subtitle v-text="body" />
+    </v-list-item-header>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  name: 'item',
+  props: {
+    icon: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+    body: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.f-item {
+  padding: 4px 8px !important;
+  user-select: none;
+  .v-list-item-avatar {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-inline-end: 8px;
+  }
+}
+</style>
